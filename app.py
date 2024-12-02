1f076276a01da2c340aeb77c80cb4a5c1842e978
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-12-02 20:02:33 -0300

    classs products data bases

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #importação
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,6 +15,14 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    product = Product(name=data["name"],price=data["price"],description=data.get("description", ""))
+    db.session.add(product)
+    db.session.commit()
+    return "produto cadastrado com sucesso!"
 
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
