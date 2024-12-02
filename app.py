258b8860f0f504092ead5d56a6c8fae152c4d4f9
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-12-02 00:34:51 -0300

    criando o banco de dados:ecommerce.db

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ db = SQLAlchemy(app)
 
 # Modelagem
 # Produto(id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
 def hello_world():
@@ -16,3 +23,10 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#criando o banco dados: new terminal: flask shell
+#depois: db.create_all()
+#depois: db.session.commit()
+# a session é a conexao com o db, o commit efetiva as mudanças enviando para o banco dados
+#por fim: exit()
+#vai ser criado  a pasta instance, e o arquivo de extensao .db
