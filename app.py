f2d51c2020d9eab946457c329609dc9160f03bc5
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2024-12-02 00:05:40 -0300

    acrescentando swagger.yaml e  definindo SQLite: banco de dados

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 #importação
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+db = SQLAlchemy(app)
+
+# Modelagem
+# Produto(id, name, price, description)
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
 def hello_world():
